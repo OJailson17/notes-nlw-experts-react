de075151a48be008deabe34df06d127db60a1eff
OJailson17 <JaylsonO17@gmail.com>
2024-02-07 00:58:05 -0300

    feat: create new note

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo.svg';
 import { NewNoteCard } from './components/new-note-card';
 import { NoteCard } from './components/note-card';
 
+const note = {
+	date: new Date(),
+	content: 'Hello World',
+};
+
 export function App() {
 	return (
 		<div className='mx-auto max-w-6xl my-12 space-y-6'>
@@ -20,9 +25,7 @@ export function App() {
 			<div className='grid gap-6 grid-cols-3 auto-rows-[250px]'>
 				<NewNoteCard />
 
-				<NoteCard />
-				<NoteCard />
-				<NoteCard />
+				<NoteCard note={note} />
 			</div>
 		</div>
 	);
