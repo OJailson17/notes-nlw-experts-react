925e2c19098f4b35a36e94014c0d24964077d3a7
OJailson17 <JaylsonO17@gmail.com>
2024-02-09 04:04:26 -0300

    style: make layout responsive

--- src/app.tsx
+++ src/app.tsx
@@ -48,7 +48,7 @@ export function App() {
 			: notes;
 
 	return (
-		<div className='mx-auto max-w-6xl my-12 space-y-6'>
+		<div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
 			<img src={logo} alt='NLW Experts' />
 
 			<form className='w-full mt-6'>
@@ -62,7 +62,7 @@ export function App() {
 
 			<div className='h-px bg-slate-700' />
 
-			<div className='grid gap-6 grid-cols-3 auto-rows-[250px]'>
+			<div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px]'>
 				<NewNoteCard onNoteCreated={onNoteCreated} />
 
 				{filteredNotes.map(note => (
